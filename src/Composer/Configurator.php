f0eb939f5a24427b67891f6d7d8e9db827679ce2
francoisvaillant <frvaillant@gmail.com>
2021-09-22 00:35:03 +0200

    try without composer

--- src/Composer/Configurator.php
+++ src/Composer/Configurator.php
@@ -24,23 +24,8 @@ class Configurator
         $composer = $event->getComposer();
         $IO = $event->getIo();
         $library = (int)$IO->ask('Wich map Library do you want to use ? (1 : leaflet, 2 : googlemaps, 3 : mapbox, 4 : openlayer, 0 for all)');
-        $config = new Config();
-        $source = new Config\JsonConfigSource();
 
-        //$source->addConfigSetting('require', ['symfony/twig-bundle' => '^4.4.17|^5.0'] );
-
-        if($library === 1) {
-            $source->addConfigSetting('require', ['symfony/http-kernel' => '^4.4.17|^5.0'] );
-        }
-        if($library === 2) {
-            $source->addConfigSetting('require', ['symfony/dependency-injection' => '^4.4.17|^5.0'] );
-        }
-        if($library === 3) {
-            $source->addConfigSetting('require', ['symfony/config' => '^4.4.17|^5.0'] );
-        }
-
-        $config->setConfigSource($source);
-        $composer->setConfig($config);
+        $IO->write('You chose libraby ' . self::LIBRARIES[$library]);
     }
 
     public static function postInstall(PackageEvent $event)
